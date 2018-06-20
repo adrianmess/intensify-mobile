c8eae33d94701cbc24e4f7eebddce471418faac2
Adrian Messado <adrian.messado@gmail.com>
2018-06-20 13:39:21 -0700

    create login and sign-in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
