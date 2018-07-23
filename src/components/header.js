0d9473b78cc3a73cfb5e75a63bd4b521058ec684
Adrian Messado <adrian.messado@gmail.com>
2018-07-23 14:12:37 -0700

    fill out boilerplate for header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// Import Libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+
+//Make a component
+const Header = () => {
+  return <Text>Intensify</Text>;
+};
+
+
+//Make component available to other parts of the app
+export default Header;
