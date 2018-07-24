6731c90b955e42abce5b8644054381ba788d2d04
Adrian Messado <adrian.messado@gmail.com>
2018-07-24 09:37:47 -0700

    add filler information on muscle group

--- src/components/muscle-groups.js
+++ src/components/muscle-groups.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-const MuscleGroups = () => {
-  const { textStyle } = styles;
+class MuscleGroups extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MuscleGroups</Text>
+        <Text>MuscleGroups</Text>
+        <Text>MuscleGroups</Text>
+        <Text>MuscleGroups</Text>
+      </View>
+    );
+  }
+}
 
-  return <Text style={muscleGroup}>Test</Text>;
 
-};
-
-
-
-const styles = {
-  muscleGroup: {
-    fontSize: 20,
-    color: 'white',
-  },
-};
 
 
 export default MuscleGroups;
