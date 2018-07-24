27d2a7e3e406553baf0ec9f1380c0a4b73c45210
Adrian Messado <adrian.messado@gmail.com>
2018-07-24 10:22:54 -0700

    actually add other components to router component, change back to backs as \'back\' is reserved by router

--- src/Router.js
+++ src/Router.js
@@ -3,8 +3,9 @@ import { Scene, Router } from 'react-native-router-flux';
 import Welcome from './components/welcome';
 import MuscleGroups from './components/muscle-groups';
 import Arms from './components/arms';
-import Back from './components/back';
+import Backs from './components/backs';
 import Chest from './components/chest';
+import Legs from './components/legs';
 import Shoulders from './components/shoulders';
 
 
@@ -15,6 +16,10 @@ const RouterComponent = () => {
         <Scene key="welcome" component={Welcome}  hideNavBar={true} initial />
         <Scene key="musclegroups" component={MuscleGroups}  />
         <Scene key="arms" component={Arms}  />
+        <Scene key="backs" component={Backs}  />
+        <Scene key="chest" component={Chest}  />
+        <Scene key="legs" component={Legs}  />
+        <Scene key="shoulders" component={Shoulders}  />
        </Scene>
     </Router>
   );
