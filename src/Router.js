7d71efe34b254ce18904f84406977d45b2a0f61c
Adrian Messado <adrian.messado@gmail.com>
2018-07-24 10:15:46 -0700

    add muscle group components as scenes to router component

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Welcome from './components/welcome';
 import MuscleGroups from './components/muscle-groups';
+import Arms from './components/arms';
+import Back from './components/back';
+import Chest from './components/chest';
+import Shoulders from './components/shoulders';
+
 
 const RouterComponent = () => {
   return (
@@ -9,6 +14,7 @@ const RouterComponent = () => {
       <Scene key="root">
         <Scene key="welcome" component={Welcome}  hideNavBar={true} initial />
         <Scene key="musclegroups" component={MuscleGroups}  />
+        <Scene key="arms" component={Arms}  />
        </Scene>
     </Router>
   );
