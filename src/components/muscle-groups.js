c360c6c859de33ecda572522cfee4387911ba003
Adrian Messado <adrian.messado@gmail.com>
2018-07-24 09:56:04 -0700

    change text to 5 diff muscle group names, add styling and flex

--- src/components/muscle-groups.js
+++ src/components/muscle-groups.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 class MuscleGroups extends Component {
   render() {
     return (
-      <View>
-        <Text>MuscleGroups</Text>
-        <Text>MuscleGroups</Text>
-        <Text>MuscleGroups</Text>
-        <Text>MuscleGroups</Text>
-      </View>
+      <LinearGradient colors={['#abd264', '#0ab8af']} style={styles.linearGradient}>
+      <View style={styles.muscleContainer}>
+          <Text style={styles.muscleGrpTxt}>Arms</Text>
+          <Text style={styles.muscleGrpTxt}>Back</Text>
+          <Text style={styles.muscleGrpTxt}>Chest</Text>
+          <Text style={styles.muscleGrpTxt}>Legs</Text>
+          <Text style={styles.muscleGrpTxt}>Shoulders</Text>
+        </View>
+      </LinearGradient>
     );
   }
 }
+
+const styles = StyleSheet.create ({
+  linearGradient: {
+    flex: 1,
+  },
+  muscleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  muscleGrpTxt: {
+    color: 'white',
+    margin: 10,
+  },
+});
 
 
 
