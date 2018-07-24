c351d7a7fad98b9030d625c730404b25fd2dd8da
Adrian Messado <adrian.messado@gmail.com>
2018-07-24 09:38:45 -0700

    add option to hide navigation bar to Scene tag

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Welcome from './components/welcome';
+import MuscleGroups from './components/muscle-groups';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="welcome" component={Welcome} title="Welcome" />
+        <Scene key="welcome" component={Welcome}  hideNavBar={true} initial />
+        <Scene key="musclegroups" component={MuscleGroups}  />
        </Scene>
     </Router>
   );
