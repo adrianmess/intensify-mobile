778c4bef34ab29675465fa1f716b6527ede1f8f0
Adrian Messado <adrian.messado@gmail.com>
2018-07-25 21:11:58 -0700

    import FlatList from react-native, test FlatList to list json entries as list

--- intensify-app/src/components/arms.js
+++ intensify-app/src/components/arms.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-class Arms extends Component {
+
+const armsdata = require ('../../assets/basic.json');
+
+export default class Arms extends Component {
+  constructor()
+  { super();
+  this.state= armsdata
+}
   render() {
+
     return (
       <LinearGradient colors={['#abd264', '#0ab8af']} style={styles.linearGradientArms}>
       <View style={styles.armsContainer}>
           <Text style={styles.armsGrpTxt}>Triceps</Text>
           <Text style={styles.armsGrpTxt}>Biceps</Text>
           <Text style={styles.armsGrpTxt}>Forearms</Text>
-
+          <Text key={armsdata.arms.biceps} style={styles.armsGrpTxt}>{armsdata.arms.biceps.exname}</Text>
+          <Flatlist
+            data={this.state.biceps}
+            renderItem={({item}) =>
+            <Text>{item.biceps}</Text>
+            }
+            />
         </View>
       </LinearGradient>
     );
@@ -31,8 +45,3 @@ const styles = StyleSheet.create ({
     margin: 10,
   },
 });
-
-
-
-
-export default Arms;
