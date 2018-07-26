c0e95173e8b99db526d57addeab2352368a9c7e6
Adrian Messado <adrian.messado@gmail.com>
2018-07-25 21:10:32 -0700

    create workoutlist test component and test it's working

---
+++ intensify-app/src/components/WorkoutList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WorkoutList = () => {
+  return (
+    <View>
+      <Text>Workout List!</Text>
+    </View>
+  )
+}
+
+export default WorkoutList;
